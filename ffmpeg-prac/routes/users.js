fee1424f11b931174aac0ff39b74fa1680660916
meanjung <meanjunghong@naver.com>
2020-12-15 12:37:36 +0900

    update : get -> post

--- ffmpeg-prac/routes/users.js
+++ ffmpeg-prac/routes/users.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 const productController = require('../controller/productController');
 
-router.get('/ffmpeg', productController.instaRegister);
+router.post('/ffmpeg', productController.instaRegister);
 
 module.exports = router;
